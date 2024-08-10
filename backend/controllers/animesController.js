089aab56b45ca43994f64c02b0bbcd77c5d4ae7c
Wwright91 <wisdomwright-martin@pursuit.org>
2024-08-10 11:21:15 -0400

    /animes
    1. Get all animes route complete

--- backend/controllers/animesController.js
+++ backend/controllers/animesController.js
@@ -24,6 +24,17 @@ const {
 //   }
 // ]
 
+animes.get("/", async (req, res) => {
+  try {
+    const allAnimes = await getAllAnimes();
+    if (allAnimes[0]) {
+      res.status(200).json(allAnimes);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
 //Write a POST route that takes user provided data from the request body and creates a new anime in the database. The route should respond with a 201 status code and the new anime.
 //if the request body does not contain a name and description, or if the body's name or description have no length, respond with an error
 //your response body should look this:
