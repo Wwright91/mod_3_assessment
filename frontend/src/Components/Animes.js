af40f4f3b62ef055b2056c3506b25a43b378a1b4
Wwright91 <wisdomwright-martin@pursuit.org>
2024-09-04 20:19:05 -0400

    Render list of anime
    1. Installed Axios
    2. Fetched data from the API
    3. Rendered list of anime
    
    All tests passed.

--- frontend/src/Components/Animes.js
+++ frontend/src/Components/Animes.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import Anime from "./Anime";
+import axios from "axios";
+
+const  API = process.env.REACT_APP_API_URL;
 
 function Animes() {
   //fetch all animes from the backend and render them as a list using the Anime component. Make sure to style the animes to look like the screenshot from the README. Feel free to use axios to grab data
 
+  const [animes, setAnimes] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API}/animes`).then((res) => {
+      setAnimes(res.data);
+    });
+  }, []);
+
   return (
     <section className="index" id="anime-list">
-      <p>No animes to display</p>
+      {animes.map(({id, name, description}) => {
+        return <Anime key={id} name={name} description={description} />;
+      })}
     </section>
   );
 }
